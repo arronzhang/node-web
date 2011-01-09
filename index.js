824ddebe7318bc6cfdc7118897c3c3ef9600eab8
Hidden <zzdhidden@gmail.com>
2011-01-09 17:26:12 +0800

    Add sax-js xml2js

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ require.paths.unshift(__dirname + "/supports");
 require.paths.unshift(__dirname + "/supports/node-mongodb-native/lib");
 require.paths.unshift(__dirname + "/supports/connect/lib");
 require.paths.unshift(__dirname + "/supports/mongoose");
+require.paths.unshift(__dirname + "/supports/sax-js/lib");
+require.paths.unshift(__dirname + "/supports/xml2js/lib");
