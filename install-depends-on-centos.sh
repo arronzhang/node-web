146bdb479b59d7158da92c6bec49fd1f6ab23a1f
Hidden <zzdhidden@gmail.com>
2011-04-29 23:43:17 +0800

    For centos

---
+++ install-depends-on-centos.sh
@@ -0,0 +1,4 @@
+yum -y install openssl-devel
+yum -y install git tcsh scons gcc-c++ glibc-devel
+yum -y install boost-devel pcre-devel js-devel readline-devel
+yum -y install boost-devel-static readline-static ncurses-static
